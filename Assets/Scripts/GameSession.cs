6b777559c543a5dd6aab874af5eefe498d2da28b
Ryan Wood <woodr1991@googlemail.com>
2021-12-06 18:11:43 +0000

    Scene persist added - coins added

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI ScoreText;
     void Awake() 
     {
         int numGameSession = FindObjectsOfType<GameSession>().Length;
@@ -18,6 +22,12 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start() 
+    {
+        livesText.text = playerLives.ToString();
+        ScoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -37,11 +47,19 @@ public class GameSession : MonoBehaviour
         playerLives--;
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
+        livesText.text = playerLives.ToString();
     }
 
     void ResetGameSession()
     {
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(0);
         Destroy(gameObject);
+    }
+
+    public void AddScore(int valueOfPickup)
+    {
+        score += valueOfPickup;
+        ScoreText.text = score.ToString();
     }
 }
